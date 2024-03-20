7749dda870f9a3012fc9b922c93ed3702ea4407f
Adnan <adnanvandyck@gmail.com>
2024-03-20 06:09:29 -0400

    added Store Listings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Background from './components/Background';
+import StoreListings from './components/StoreListings';
 import TopBar from './components/TopBar';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <TopBar />
       <Background />
+      <StoreListings />
     </div>
   );
 }
