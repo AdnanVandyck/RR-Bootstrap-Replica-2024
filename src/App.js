9e91eb5923b1e4d8ce399580adc75832bad60043
Adnan <adnanvandyck@gmail.com>
2024-03-20 05:00:05 -0400

    added background component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Background from './components/Background';
 import TopBar from './components/TopBar';
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <Background />
     </div>
   );
 }
