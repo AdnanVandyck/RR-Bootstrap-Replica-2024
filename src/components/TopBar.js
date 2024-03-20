777a1355371cacb8efcad97bb9cf2701f307c37f
Adnan <adnanvandyck@gmail.com>
2024-03-20 04:08:23 -0400

    added top bar to main app

---
+++ src/components/TopBar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Navbar, Nav, Button } from 'react-bootstrap'
+
+function TopBar() {
+  return (
+    <Navbar bg="light" expand="lg" >
+        <Navbar.Brand style={{margin: "5px 0 0 20px"}} href="#home">
+            <img src="https://d2guulkeunn7d8.cloudfront.net/assets/beetstrap/brand/instacart-logo-color-4db9d81ca0b7638befdc4bd331f64a2633df790c0b55ef627c99b1ba77af72b7.svg" alt="Instacart logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+            </Nav>
+            <Nav>
+                <Nav.Link href="#LogIn">Log In</Nav.Link>
+                <Button variant="success">Sign Up</Button>{' '}
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+export default TopBar
